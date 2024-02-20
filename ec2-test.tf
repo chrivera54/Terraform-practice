c07692400f57980312c78e59f71abc12e13e82cc
chrivera54 <kamilo.rivera12@gmail.com>
2024-02-20 11:57:35 -0500

    agregar archivos tf desde VSC al repositorio Git

--- ec2-test.tf
+++ ec2-test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "app_server" {
   ami           = "ami-0cf10cdf9fcd62d37"
   instance_type = "t2.micro"
-  subnet_id = aws_subnet.public_subnet_1.id
+  subnet_id     = aws_subnet.public_subnet_1.id
 
   tags = {
     Name = "ExampleAppServerInstance"
--- vpc.tf
+++ vpc.tf
@@ -5,15 +5,15 @@ resource "aws_vpc" "my_vpc_terraform" {
 
 # Crear subredes públicas
 resource "aws_subnet" "public_subnet_1" {
-  vpc_id                  = aws_vpc.my_vpc_terraform.id
-  cidr_block              = "10.0.0.0/25" # Cambia esto a tu rango de direcciones IP CIDR preferido
-  availability_zone       = "us-east-1a" # Cambia esto a tu zona de disponibilidad preferida
+  vpc_id            = aws_vpc.my_vpc_terraform.id
+  cidr_block        = "10.0.0.0/25" # Cambia esto a tu rango de direcciones IP CIDR preferido
+  availability_zone = "us-east-1a"  # Cambia esto a tu zona de disponibilidad preferida
 }
 
 resource "aws_subnet" "public_subnet_2" {
-  vpc_id                  = aws_vpc.my_vpc_terraform.id
-  cidr_block              = "10.0.0.128/25" # Cambia esto a tu rango de direcciones IP CIDR preferido
-  availability_zone       = "us-east-1b" # Cambia esto a tu zona de disponibilidad preferida
+  vpc_id            = aws_vpc.my_vpc_terraform.id
+  cidr_block        = "10.0.0.128/25" # Cambia esto a tu rango de direcciones IP CIDR preferido
+  availability_zone = "us-east-1b"    # Cambia esto a tu zona de disponibilidad preferida
 }
 
 # Crear Internet Gateway
